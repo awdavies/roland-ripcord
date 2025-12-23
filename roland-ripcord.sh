39f4878ad805968f62f5e322c2072f63b06948e6
Andrew Davies <a.w.davies.vio@gmail.com>
2025-12-22 23:45:55 -0800

    A few minor changes.
    -- Added trap to handle loop exiting properly.
    -- Continue to retry connecting even if --dump fails.

--- roland-ripcord.sh
+++ roland-ripcord.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+trap "echo 'received SIGINT. Quitting.'; exit" SIGINT
+
 function parse_j6_device() {
 	local devices
 	devices=$(amidi -l | tail -n +2)
@@ -12,8 +14,7 @@ while true; do
 	J6_DEV=$(parse_j6_device)
 	if [[ -n ${J6_DEV} ]]; then
 		echo "Found it. Dumping at: ${J6_DEV}"
-		amidi -p "${J6_DEV}" -c --dump
-		break
+		amidi -p "${J6_DEV}" --dump
 	fi
 	sleep 0.5
 done
