8a4eaae9f7f4b87e158af65b9b272ce8c3d77531
Andrew Davies <a.w.davies.vio@gmail.com>
2025-12-22 22:34:29 -0800

    Initial commit.
    Contains the worst possible implementation ever.

---
+++ roland-ripcord.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+function parse_j6_device() {
+	local devices
+	devices=$(amidi -l | tail -n +2)
+	if [[ -n ${devices} && ${devices} =~ "J-6 MIDI OUT"$ ]]; then
+		echo "${devices}" | awk '{ print $2; }'
+	fi
+}
+
+J6_DEV=""
+while true; do
+	J6_DEV=$(parse_j6_device)
+	if [[ -n ${J6_DEV} ]]; then
+		echo "Found it. Dumping at: ${J6_DEV}"
+		amidi -p "${J6_DEV}" -c --dump
+		break
+	fi
+	sleep 0.5
+done
