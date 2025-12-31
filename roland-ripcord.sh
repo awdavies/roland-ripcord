8e49678c5052a50a50903fafdf1226fdad695247
Andrew Davies <a.w.davies.vio@gmail.com>
2025-12-31 01:05:12 -0800

    Make auto-connect to Midi Through the default
    No sense making the user type it in themselves.

--- roland-ripcord.sh
+++ roland-ripcord.sh
@@ -23,12 +23,12 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-CMD=$*
-if [[ -z ${CMD} ]]; then
-	trap "echo 'received SIGINT. Quitting.'; exit" SIGINT
-else
-	trap "echo 'received SIGINT. Quitting then running command \"${CMD}\".'; ${CMD}; exit" SIGINT
-fi
+trap "echo 'received SIGINT. Quitting.'; exit" SIGINT
+
+function quit_with_err() {
+	echo $* >&2
+	exit 1
+}
 
 function parse_j6_device() {
 	local devices
@@ -39,16 +39,87 @@ function parse_j6_device() {
 	while IFS= read -r dev; do
 		if [[ "${dev}" =~ "J-6 MIDI OUT"$ ]]; then
 			echo "${dev}" | awk '{ print $2; }'
+			break
 		fi
 	done <<< "${devices}"
 }
 
+function parse_j6_device_aconnect() {
+	local devices
+	devices=$(aconnect -i)
+	if [[ -z ${devices} ]]; then
+		quit_with_err "We could not find any input devices."
+	fi
+	while IFS= read -r dev; do
+		if [[ "${dev}" =~ "J-6" ]]; then
+			echo "${dev}" | awk '{ sub(/:/, ""); print $2; }'
+			break
+		fi
+	done <<< "${devices}"
+}
+
+function parse_midi_through() {
+	# Is this mostly copy-pasted from above? Yes.
+	local devices
+	devices=$(aconnect -o)
+	if [[ -z ${devices} ]]; then
+		quit_with_err "We could not find any output devices."
+	fi
+	while IFS= read -r dev; do
+		if [[ "${dev}" =~ "Midi Through" ]]; then
+			# We're making some load bearing assumptions here:
+			# -- One midi device.
+			# -- The format isn't gonna change for `aconnect -o`
+			#    (probably, right?)
+			# -- Port isn't going to be something other than zero.
+			# -- Probably something else, I dunno.
+			echo "${dev}" | awk '{ sub(/:/, ""); print $2; }'
+			break
+		fi
+	done <<< "${devices}"
+}
+
+MIDI_THROUGH_IFACE="$(parse_midi_through)"
+if [[ -z "$MIDI_THROUGH_IFACE" ]]; then
+	quit_with_err "Could not find Midi Through interface"
+fi
 J6_DEV=""
 while true; do
 	J6_DEV=$(parse_j6_device)
 	if [[ -n ${J6_DEV} ]]; then
-		echo "Found it. Dumping: ${J6_DEV}"
-		amidi -p "${J6_DEV}" --dump
+		echo "Found at: ${J6_DEV}"
+		echo "Now going to verify liveness."
+		printf "Waiting for ten clock cycles (one dot per cycle)"
+		# Occasionally we can expect CF 00 (I believe this is
+		# program-change channel 16 to 0?).
+		# According to https://midi.org/expanded-midi-1-0-messages-list
+		#
+		# however this isn't consistent (I usually have to click a
+		# button for that (the horror)).
+		#
+		# Instead we'll do something silly: we'll wait for ten clock
+		# messages from the device, which should be enough to know it's
+		# alive, then connect it to the midi through device.
+		CLOCK_COUNT=0
+		amidi -p "${J6_DEV}" -c --dump | while read -r amidi_line; do
+			if [[ "${amidi_line}" = "F8" ]]; then
+				CLOCK_COUNT=$((CLOCK_COUNT+1))
+				printf "."
+			fi
+
+			if [ "${CLOCK_COUNT}" -eq 10 ]; then
+				break
+			fi
+		done
+		J6_IFACE="$(parse_j6_device_aconnect)"
+		if [[ -z "$J6_IFACE" ]]; then
+			echo "Could not find device. Likely disconnected. Waiting again."
+			continue
+		fi
+		printf "\nConnecting ${J6_IFACE}:0 to ${MIDI_THROUGH_IFACE}:0\n"
+		aconnect "${J6_IFACE}:0" "${MIDI_THROUGH_IFACE}:0" || exit 1
+		echo "Done"
+		exit 0
 	fi
 	sleep 0.5
 done
